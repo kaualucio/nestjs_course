8476f6be8a44dc27bc3afd665db20e01ad46e0ae
Your Name <you@example.com>
2024-06-25 09:53:52 -0300

    feat: add jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,19 @@ import { UserEntity } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLoginDto } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from '../user/dtos/returnUser.dto';
+import { LoginPayloadDto } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto): Promise<UserEntity> {
+  async login(loginDto: LoginDto): Promise<ReturnLoginDto> {
     const user: UserEntity | undefined = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
@@ -23,6 +30,11 @@ export class AuthService {
       throw new NotFoundException('Email e/ou senha inválidos.');
     }
 
-    return user;
+    const access_token = this.jwtService.sign({ ...new LoginPayloadDto(user) });
+
+    return {
+      access_token,
+      user: new ReturnUserDto(user),
+    };
   }
 }
