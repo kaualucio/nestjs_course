99ba126ed67fe7a6d48db47576ec9f948e3059ee
Your Name <you@example.com>
2024-06-25 19:12:10 -0300

    feat: create decorator userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -13,7 +12,9 @@ import { UserService } from 'src/user/user.service';
 import { CityService } from 'src/city/city.service';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enums/user-type.enum';
+import { UserId } from 'src/decorators/user-id.decorator';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(
@@ -22,12 +23,11 @@ export class AddressController {
     private readonly cityService: CityService,
   ) {}
 
-  @Post('/:userId')
-  @Roles(UserType.User)
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number,
+    @UserId() userId: number,
   ): Promise<AddressEntity> {
     await this.userService.findUserById(userId);
     await this.cityService.findeCityById(createAddressDto.city_id);
---
+++ src/decorators/user-id.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { authorizationToLoginPayload } from 'src/utils/base-64-converter';
+
+export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
+  const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+  const loginPayload = authorizationToLoginPayload(authorization);
+
+  // GqlExecutionContext.create(ctx).getContext().user;
+  return loginPayload?.id;
+});
