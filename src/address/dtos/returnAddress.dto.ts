e322e89d54ee1b8e8a28663e81eac77d8d5c9740
Your Name <you@example.com>
2024-06-25 08:25:50 -0300

    feat: insert DTO's for city and state

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,16 @@
+import { ReturnCityDto } from '../../city/dtos/returnCity.dto';
 import { AddressEntity } from '../entities/address.entity';
 
 export class ReturnAddressDto {
   complement: string;
   number_address: number;
   cep: string;
+  city?: ReturnCityDto;
 
   constructor(address: AddressEntity) {
     this.complement = address.complement;
     this.number_address = address.number_address;
     this.cep = address.cep;
+    this.city = address.city ? new ReturnCityDto(address.city) : undefined;
   }
 }
